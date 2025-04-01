e1842ece02b97a3a4d81b5cc8159fdb30b785a65
Owen Clarke <oc8825@princeton.edu>
2025-03-31 20:35:04 -0400

    dereferencing onRoot

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -100,7 +100,6 @@ boolean CheckerDT_isValid(boolean bIsInitialized, Node_T oNRoot,
          fprintf(stderr, "Not initialized, but count is not 0\n");
          return FALSE;
       }
-
       if (oNRoot != NULL)
       {
          fprintf(stderr, "Not initialized, but root is not NULL\n");
@@ -108,13 +107,11 @@ boolean CheckerDT_isValid(boolean bIsInitialized, Node_T oNRoot,
       }
    }
 
-   /*
-   if (Node_getParent(oNRoot) != NULL)
+   if (Node_getParent(*oNRoot) != NULL)
    {
       fprintf(stderr, "Root has a parent\n");
       return FALSE;
    }
-   */
 
    /* Now checks invariants recursively at each node from the root. */
    return CheckerDT_treeCheck(oNRoot);
