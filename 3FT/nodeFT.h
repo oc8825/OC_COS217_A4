c0da4d63c2ab5971bb5baecd4bc19945b8600ff6
Owen Clarke <oc8825@princeton.edu>
2025-04-10 16:15:15 -0400

    commenting node header

--- 3FT/nodeFT.h
+++ 3FT/nodeFT.h
@@ -15,7 +15,13 @@ typedef struct node *Node_T;
 /*
  Creates a new node in the File Tree, with path oPPath and
  parent oNParent. Returns an int SUCCESS status and sets *poNResult
- to be the new node if successful. The Node object can be either a directory or a file, in which if it is a file the boolean isFile is set to TRUE, it has no children and its attributes relating to its contents and size are set accordingly. Otherwise, the opposite happens, its file status is set to FALSE, its children array is allocated in memory, and anything relating to its contents is set to FALSE. If it fails, then sets *poNResult to NULL
+ to be the new node if successful. The Node object can be either a
+ directory or a file, in which if it is a file the boolean isFile is
+ set to TRUE, it has no children, and its attributes relating to its
+ contents (oNContents) and size (contentSize) are set accordingly.
+ Otherwise, the opposite happens, its file status is set to FALSE, its
+ children array is allocated in memory, and anything relating to its
+ contents is set to FALSE. If it fails, then sets *poNResult to NULL
  and returns status:
  * MEMORY_ERROR if memory could not be allocated to complete request
  * CONFLICTING_PATH if oNParent's path is not an ancestor of oPPath
@@ -25,11 +31,13 @@ typedef struct node *Node_T;
  * ALREADY_IN_TREE if oNParent already has a child with this path
 */
 int Node_new(Path_T oPPath, Node_T oNParent,
-             Node_T *poNResult, boolean isFile, void *oNContents, size_t contentSize);
+             Node_T *poNResult, boolean isFile, void *oNContents,
+             size_t contentSize);
 
 /*
  Destroys and frees all memory allocated for the subtree rooted at
- oNNode, i.e., deletes this node and all its descendents only if it is a directory. If it is a file, just delete the file. Returns the
+ oNNode, i.e., deletes this node and all its descendents only if it is
+ a directory. If it is a file, just delete the file. Returns the
  number of nodes deleted.
 */
 size_t Node_free(Node_T oNNode);
@@ -39,8 +47,8 @@ Path_T Node_getPath(Node_T oNNode);
 
 /*
  Returns TRUE if oNParent has a child with path oPPath. Returns
- FALSE if it does not. Automatically returns FALSE if the Node is a file.
- If oNParent has such a child, stores in *pulChildID the child's
+ FALSE if it does not. Automatically returns FALSE if the Node is a
+ file. If oNParent has such a child, stores in *pulChildID the child's
  identifier (as used in Node_getChild). If oNParent does not have
  such a child, stores in *pulChildID the identifier that such a
  child _would_ have if inserted.
@@ -48,11 +56,13 @@ Path_T Node_getPath(Node_T oNNode);
 boolean Node_hasChild(Node_T oNParent, Path_T oPPath,
                       size_t *pulChildID);
 
-/* Returns the number of children that oNParent has, 0 automatically it is a file */
+/* Returns the number of children that oNParent has, 0 automatically
+it is a file */
 size_t Node_getNumChildren(Node_T oNParent);
 
 /*
-If the Node is a file, returns NOT_A_DIRECTORY, else behaves accordingly:
+If the Node is a file, returns NOT_A_DIRECTORY, else behaves
+accordingly:
 Returns an int SUCCESS status and sets *poNResult to be the child
  node of oNParent with identifier ulChildID, if one exists.
  Otherwise, sets *poNResult to NULL and returns status:
@@ -90,20 +100,24 @@ char *Node_toString(Node_T oNNode);
 boolean Node_isFile(Node_T oNNode);
 
 /*
-Returns the contents of the ONNode, will be NULL if oNNode is not a file
+Returns the contents of the ONNode, will be NULL if oNNode is not a
+file
 */
 void *Node_getContents(Node_T oNNode);
 
 /*
- Returns the size of the contents in ONNode, will be NULL if oNNode is not a file
+ Returns the size of the contents in ONNode, will be NULL if oNNode is
+ not a file
 */
 size_t Node_getContentsSize(Node_T oNNode);
 
 /*
-Returns the old contents of the Node and sets the new contents of the Node as well.
-Will return NULL if oNNode is not a file.
+Returns the old contents of the Node and sets the new contents of the
+Node according to newContents and newContentsLength. Will return NULL
+if oNNode is not a file.
 */
-void *Node_setContents(Node_T oNNode, void *newContents, size_t newContentsLength);
+void *Node_setContents(Node_T oNNode, void *newContents,
+                       size_t newContentsLength);
 
 /*
  Changes the isFile boolean of a oNNOde, setting it to TRUE.
